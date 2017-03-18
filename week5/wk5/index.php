fff413ad1298b0deac939f892712c97d052f58ca
Documents <Documents@Toshiba>
2017-03-17 21:05:28 -0400

    resubmit assignment 5

--- week5/wk5/index.php
+++ week5/wk5/index.php
@@ -1,15 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Lab 5: Index Page</title>
+   <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
     </head>
     <body>
+        
+         <!-- Navigation Bar -->
+        <nav class="navbar navbar-default navbar-inverse">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Lab 5:  Sites Index Page</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <br />
+
+        <!-- Main Content Container -->
+        <div class="container">
+    
         <?php
         // put your code here
         
             include './functions/dbconnect.php';
             include './functions/util.php';
+            
+            
+          
             
             $site = filter_input(INPUT_POST, 'site');
             $errors = array();
@@ -40,6 +67,8 @@
                             foreach ($siteLinks as $link) {
                                 $binds = array(":site_id" => $site_id, ":link" => $link); 
                                 $stmt->execute($binds);
+                                
+                                
                             }
                             
                             // may need to return false;
@@ -51,13 +80,25 @@
                 }
             }
         ?>
+    
         
         <?php include './templates/error-messages.php'; ?>
         
         <form action="#" method="post">
+              <select name="column">
+                    <option value="site_id" selected="selected">ID</option>
+                    <option value="site">Site</option>
+                     <option value="date">Date</option>
+                </select>
+                <input type="hidden" name="action" value="Submit" />
+                <br />
+                <input type="submit" value="Submit" />
+                <input type="hidden" name="action" value="search">
+                
             Site: <input type="text" name="site" value="<?php echo $site; ?>" />
             <br />
             <input type="submit" color="red" value="Submit" />
         </form>
+        
     </body>
 </html>
